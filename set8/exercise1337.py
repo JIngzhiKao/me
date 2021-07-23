383110122f592fedbf0e9227c71cc5910ca50361
JIngzhiKao <85300600+JIngzhiKao@users.noreply.github.com>
2021-07-23 12:27:45 +0800

    24

--- set8/exercise1337.py
+++ set8/exercise1337.py
@@ -251,12 +251,7 @@ def random_filler_text(number_of_words=200) -> str:
     
     import random
 
-    HALLOW = make_filler_text_dictionary()
-    words = []
-    for _ in range(number_of_words):
-        length = random.randint(3, 6)
-        word_index = random.randint(0, 2)
-        words.append(HALLOW[length][word_index])
+    
 
     return " ".join(words)
 
