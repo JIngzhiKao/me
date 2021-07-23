61f7a85041712e09cc4062a2564b8627e89cabf1
JIngzhiKao <85300600+JIngzhiKao@users.noreply.github.com>
2021-07-23 12:12:18 +0800

    24

--- set8/exercise1337.py
+++ set8/exercise1337.py
@@ -218,7 +218,7 @@ def make_filler_text_dictionary() -> Dict:
 
     url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength="
     wd = {}
-    for i in range(3,8):
+    for i in range(3,7):
         wd{i} = []
         for _ in range(4):
             r = requests.get(url + str(i))
